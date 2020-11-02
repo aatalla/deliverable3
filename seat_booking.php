cf1f95cc82e177afb16c9e2da04020cc8cff3c2d
OMR2K <qtr.omr@gmail.com>
2020-11-03 02:06:31 +0300

    added some comments

--- seat_booking.php
+++ seat_booking.php
@@ -9,4 +9,18 @@
 
     SQL command that filters based on ?id=x
 
+    PAGE REQUIREMENTS:
+    
+    After Book now link/button is clicked this should display all the categories with
+    the following details
+        • Category type
+        • Price
+        • Seats Available
+        • A textbox where the user can enter the number of tickets for each category
+    
+    There should be a next/submit button to go to the next page but make sure before that
+    you did all required validations. For example, you know through research the maximum
+    number of tickets one customer can buy. Also make sure that a number was entered and
+    that does not exceed the number of available tickets.
+
  -->
