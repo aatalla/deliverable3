95eecf7749fe77354908e3709e8267522193dc66
aatalla <aatalla@andrew.cmu.edu>
2020-11-02 20:08:15 +0300

    added p tags

--- match_booking.php
+++ match_booking.php
@@ -1,11 +1,26 @@
 <html>
-    <select>
+    <select onchange = "">
         <!-- loop through all MATCH table and setting match options (php part)-->
     </select>
-    <?php $x = 10; ?>
-<label onclick="this.innerHTML = ('<?php echo $x; ?>')"> Match Number </label>
-<label> Match </label>
-<label> Stadium </label>
-<label> Date </label>
-<label> Time </label>
+    <?php /*get the match details and store them as php variables 
+          so we could echo them in their respective p tag*/  ?>
+    <label> Match Number </label>
+    <p id="matchnumber"></p>
+    <br>
+    <br>
+    <label> Match </label>
+    <p id="match"></p>
+    <br>
+    <br>
+    <label> Stadium </label>
+    <p id="stadium"></p>
+    <br>
+    <br>
+    <label> Date </label>
+    <p id="date"></p>
+    <br>
+    <br>
+    <label id=> Time </label>
+    <p id="time"></p>
+
 </html>
