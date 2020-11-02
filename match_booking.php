e59914295cf3470d61768f3af5fb4e36970b27a3
aatalla <aatalla@andrew.cmu.edu>
2020-11-02 19:57:38 +0300

    fixed label not showing

--- match_booking.php
+++ match_booking.php
@@ -1,5 +1,11 @@
 <html>
     <select>
-        <!-- loop through all MATCH table and setting match options-->
+        <!-- loop through all MATCH table and setting match options (php part)-->
     </select>
+    <?php $x = 10; ?>
+<label onclick="this.innerHTML = ('<?php echo $x; ?>')"> Match Number </label>
+<label> Match </label>
+<label> Stadium </label>
+<label> Date </label>
+<label> Time </label>
 </html>
