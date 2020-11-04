c50c0dd1ae0e89f7feea6df3110c35feb6351b78
aatalla <aatalla@andrew.cmu.edu>
2020-11-04 18:22:12 +0300

    test

--- match_booking.php
+++ match_booking.php
@@ -15,6 +15,7 @@ $dbname = "Team2X_Project";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
+  echo "hello";
   die("Connection failed: " . $conn->connect_error);
 }
 
