e8d83366ad02dc6553d34ee96e2a4f7e1611d011
aatalla <aatalla@andrew.cmu.edu>
2020-11-04 22:13:05 +0300

    populating data for customer, ccdetails, stadium

---
+++ populating.sql
@@ -0,0 +1,34 @@
+/* populating customer */
+
+insert into customer ("2000-09-20", 77055835, 1, "aatalla@andrew.cmu.edu", "Egypt", "Andria", "Atalla", "password", "elif")
+insert into customer ("2000-01-2", 12345678, 2, "a@andrew.cmu.edu", "Cambodia", "Abcd", "Efgh", "password", "elif")
+insert into customer ("2000-02-3", 87654321, 3, "b@andrew.cmu.edu", "Vietnam", "Ijkl", "Mnop", "password", "elif")
+insert into customer ("2000-03-4", 22471628, 4, "c@andrew.cmu.edu", "United States", "Qrst", "Uvwx", "password", "elif")
+insert into customer ("2000-04-5", 13082260, 5, "d@andrew.cmu.edu", "England", "Yzab", "cdef", "password", "elif")
+insert into customer ("2000-05-6", 24561234, 6, "e@andrew.cmu.edu", "Qatar", "ghij", "klmn", "password", "elif")
+insert into customer ("2000-06-7", 00112233, 7, "f@andrew.cmu.edu", "Scotland", "opqr", "stuv", "password", "elif")
+insert into customer ("2000-07-8", 44556677, 8, "g@andrew.cmu.edu", "England", "wxyz", "abcd", "password", "elif")
+insert into customer ("2000-08-9", 88991010, 9, "h@andrew.cmu.edu", "Wales", "efgh", "ijkl", "password", "elif")
+insert into customer ("2000-10-10", 11111212, 10, "i@andrew.cmu.edu", "Canada", "mnop", "qrst", "password", "elif")
+
+/*populating CCDetails*/
+insert into CCDetails ("Visa", 1111111111111111, "abcd", "efgh", "12", "2021", 3)
+insert into CCDetails ("Visa", 2222222222222222, "abcd", "efgh", "12", "2021", 4)
+insert into CCDetails ("Visa", 3333333333333333, "abcd", "efgh", "12", "2021", 5)
+insert into CCDetails ("Visa", 4444444444444444, "abcd", "efgh", "12", "2021", 6)
+insert into CCDetails ("Visa", 5555555555555555, "abcd", "efgh", "12", "2021", 7)
+insert into CCDetails ("Visa", 6666666666666666, "abcd", "efgh", "12", "2021", 8)
+insert into CCDetails ("Visa", 7777777777777777, "abcd", "efgh", "12", "2021", 9)
+insert into CCDetails ("Visa", 8888888888888888, "abcd", "efgh", "12", "2021", 10)
+insert into CCDetails ("Visa", 9999999999999999, "abcd", "efgh", "12", "2021", 1)
+insert into CCDetails ("Visa", 0000000000000000, "abcd", "efgh", "12", "2021", 2)
+
+/* populating stadium */
+insert into stadium (2000, 6000, "Lusail Stadium", "lusail", "lusail", 2000, 6000)
+insert into stadium (2000, 6000, "Al Bayt Stadium", "al khor", "al khor", 2000, 6000)
+insert into stadium (2000, 6000, "Al Janoub Stadium", "al wakrah", "al wakrah", 2000, 6000)
+insert into stadium (2000, 6000, "Al Rayyan Stadium", "al rayyan", "al rayyan", 2000, 6000)
+insert into stadium (2000, 6000, "Khalifa International Stadium", "doha", "doha", 2000, 6000)
+insert into stadium (2000, 6000, "Education City Stadium", "doha", "doha", 2000, 6000)
+insert into stadium (2000, 6000, "Ras Abu Aboud Stadium", "ras abu aboud", "ras abu aboud", 2000, 6000)
+insert into stadium (2000, 6000, "Al Thumama Stadium", "doha", "doha", 2000, 6000)
