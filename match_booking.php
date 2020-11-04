47852ce23fa918812978f9ba1708d107d82254bd
OMR2K <qtr.omr@gmail.com>
2020-11-04 18:54:41 +0300

    updated match_booking

--- match_booking.php
+++ match_booking.php
@@ -1,8 +1,65 @@
+<!DOCTYPE html>
+
 <html>
-    <title>
-        Worldcup 2022 - Match Booking
-    </title>
-</html>
+
+<style>
+
+    /* footer (copyright) style */
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+
+    /* button styles */
+    .buttons {
+        color: white;
+        font-size: 24px;
+        border-radius: 8px;
+    }
+
+    /* shadow effect on button hover */
+    .buttons:hover {
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    }
+
+    /* aligns the buttons to the center */
+    .center {
+        text-align: center;
+    }
+
+</style>
+
+<head>
+<title> Worldcup 2022 - Homepage </title>
+</head>
+
+<body>
+
+<p style="text-align: center; font-size: 36px;"> <b>World Cup 2022 Booking Website</b> </p>
+
+<hr>
+
+<!-- Aligning the buttons, and making them clickable -->
+<div class="center">
+    <button class="buttons"><a href="home.html">Home</a></button>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <button class="buttons"><a href="match_booking.php">Book Match Ticket</a></button>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <button class="buttons"><a href="home.html">Book Team Ticket</a></button>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <button class="buttons"><a href="home.html">Book Venue Ticket</a></button>
+</div>
+
+</body>
 
 <?php
 
@@ -46,6 +103,16 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
+
+<!-- Copyright section -->
+<footer class="footer">
+    <hr>
+    <p>Copyright Team 2X</p>
+</footer>
+
+</html>
+
+
 
     <!-- <form action="" method = "POST">
         <select onchange = "">
