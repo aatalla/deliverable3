d8d763cff198d694766309ee3b804d581bb85503
aatalla <aatalla@andrew.cmu.edu>
2020-11-05 18:17:18 +0300

    test

--- insert_ticket_info.php
+++ insert_ticket_info.php
@@ -60,15 +60,143 @@
     $_SESSION["total_price"] = $_POST['cat1amount'] * 400 + $_POST['cat2amount'] * 300 + $_POST['cat3amount'] * 200 + $_POST['cat4amount'] * 100;
     $_SESSION["numberOfTickets"] = $sumOfTickets;
 
-    echo "<form action='confirmation.php?matchnum=" . $_GET['matchnum'] . "' method='post'>";
+//    echo "<form action='confirmation.php?matchnum=" . $_GET['matchnum'] . "' method='post'>";
 
-    while ($count < $sumOfTickets + 1)
+    while ($count < $_POST['cat1amount'] + 1)
     {
 
         echo "<h1>" . "Ticket " . $count . " Details</h1>";
-
         echo "<label>Category:</label><br>
               <input type='text' name='Category_Ticket" . $count . "' value='1' readonly><br>";
+
+        echo "<label>Guest?</label><br>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='yes'>
+              <label for='yes'>Yes</label>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='no'>
+              <label for='no'>No</label><br>";
+        
+        echo "<label>FanID:</label><br>
+              <input type='number' name='CustFanID_Ticket" . $count . "' required><br>";
+
+        echo "<label>First name:</label><br>
+              <input type='text' name='CustFName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Last name:</label><br>
+              <input type='text' name='CustLName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Date of Birth:</label><br>
+              <input type='date' name='CustDOB_Ticket" . $count . "' required><br>";
+
+        echo "<label>Nationality:</label><br>
+              <input type='text' name='CustNationality_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Address:</label><br>
+              <input type='text' name='CustAddress_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Telephone Number:</label><br>
+              <input type='number' name='CustTel_Ticket" . $count . "' required><br>";
+      
+        echo "<label>Email:</label><br>
+              <input type='text' name='CustEmail_Ticket" . $count . "' required><br>";
+
+        echo "<br>";
+
+        $count = $count + 1;
+
+    }
+
+    while ($count < $_POST['cat2amount'] + 1)
+    {
+
+        echo "<h1>" . "Ticket " . $count . " Details</h1>";
+        echo "<label>Category:</label><br>
+              <input type='text' name='Category_Ticket" . $count . "' value='2' readonly><br>";
+
+        echo "<label>Guest?</label><br>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='yes'>
+              <label for='yes'>Yes</label>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='no'>
+              <label for='no'>No</label><br>";
+        
+        echo "<label>FanID:</label><br>
+              <input type='number' name='CustFanID_Ticket" . $count . "' required><br>";
+
+        echo "<label>First name:</label><br>
+              <input type='text' name='CustFName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Last name:</label><br>
+              <input type='text' name='CustLName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Date of Birth:</label><br>
+              <input type='date' name='CustDOB_Ticket" . $count . "' required><br>";
+
+        echo "<label>Nationality:</label><br>
+              <input type='text' name='CustNationality_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Address:</label><br>
+              <input type='text' name='CustAddress_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Telephone Number:</label><br>
+              <input type='number' name='CustTel_Ticket" . $count . "' required><br>";
+      
+        echo "<label>Email:</label><br>
+              <input type='text' name='CustEmail_Ticket" . $count . "' required><br>";
+
+        echo "<br>";
+
+        $count = $count + 1;
+
+    }
+
+    while ($count < $_POST['cat3amount']  + 1)
+    {
+
+        echo "<h1>" . "Ticket " . $count . " Details</h1>";
+        echo "<label>Category:</label><br>
+              <input type='text' name='Category_Ticket" . $count . "' value='3' readonly><br>";
+
+        echo "<label>Guest?</label><br>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='yes'>
+              <label for='yes'>Yes</label>
+              <input type='radio' name='Guest_Ticket" . $count . "' id='no'>
+              <label for='no'>No</label><br>";
+        
+        echo "<label>FanID:</label><br>
+              <input type='number' name='CustFanID_Ticket" . $count . "' required><br>";
+
+        echo "<label>First name:</label><br>
+              <input type='text' name='CustFName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Last name:</label><br>
+              <input type='text' name='CustLName_Ticket" . $count . "' required><br>";
+
+        echo "<label>Date of Birth:</label><br>
+              <input type='date' name='CustDOB_Ticket" . $count . "' required><br>";
+
+        echo "<label>Nationality:</label><br>
+              <input type='text' name='CustNationality_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Address:</label><br>
+              <input type='text' name='CustAddress_Ticket" . $count . "' required><br>";
+        
+        echo "<label>Telephone Number:</label><br>
+              <input type='number' name='CustTel_Ticket" . $count . "' required><br>";
+      
+        echo "<label>Email:</label><br>
+              <input type='text' name='CustEmail_Ticket" . $count . "' required><br>";
+
+        echo "<br>";
+
+        $count = $count + 1;
+
+    }
+
+    while ($count < $_POST['cat4amount'] + 1)
+    {
+
+        echo "<h1>" . "Ticket " . $count . " Details</h1>";
+        echo "<label>Category:</label><br>
+              <input type='text' name='Category_Ticket" . $count . "' value='4' readonly><br>";
 
         echo "<label>Guest?</label><br>
               <input type='radio' name='Guest_Ticket" . $count . "' id='yes'>
