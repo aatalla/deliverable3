41b0f37f12403df93f98b361403558a7903a1660
aatalla <aatalla@andrew.cmu.edu>
2020-11-05 05:03:55 +0300

    testing

--- confirmation.php
+++ confirmation.php
@@ -1,3 +1,34 @@
 <?php
-print_r($_POST);
-?>
+echo 
+"<table width=75% border='1'>
+    <tr>
+        <td>Fan ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>DOB</td>
+        <td>Nationality</td>
+        <td>Address</td>
+        <td>Telephone Number</td>
+        <td>Email</td>
+    </tr>";
+
+foreach($_POST as $key => $value) {
+    if (substr($key, 0, 10) == "CustFanID"){
+        $new = True;
+        echo "<tr>";
+    }
+    echo
+        "<td>" . $value . "</td>" .
+        "<td>" . $value . "</td>" .
+        "<td>" . $value . "</td>" ;
+        // <td>DOB</td>
+        // <td>Nationality</td>
+        // <td>Address</td>
+        // <td>Telephone Number</td>
+        // <td>Email</td>";
+    if ($new){
+        echo "</tr>";
+        $new = False;
+    }
+}
+echo "</table>";?>
