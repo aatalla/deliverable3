fc38468db78b5af076c66ffdb75b5daeb0bbd156
aatalla <aatalla@andrew.cmu.edu>
2020-11-17 21:35:24 +0300

    ekbgeiou

--- login.php
+++ login.php
@@ -1,7 +1,13 @@
 <?php
-session_start()
-$_SESSION["login_status"] = False;
-?>
-<form>
 
+session_start();
+$_SESSION["login_status"] = False;
+
+?>
+
+<form>
+<label> Email: </label>
+<input type="email">
+<label>Password</label>
+<input type="text>">
 </form>
