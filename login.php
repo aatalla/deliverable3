bc959ec1f3b7bafd1d61cb9958a0c4cea92f91cf
aatalla <aatalla@andrew.cmu.edu>
2020-11-17 23:02:29 +0300

    hdfjvn

--- login.php
+++ login.php
@@ -18,9 +18,9 @@ die("Connection failed: " . $conn->connect_error);
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$login = "SELECT UserEmail, UserPassword password FROM USER WHERE UserEmail ='" . $email . "'" . "and UserPassword='" . $password ."';";
+$login = "SELECT UserEmail, UserPassword FROM USER WHERE UserEmail ='" . $email . "'" . "and UserPassword='" . $password ."';";
 $result = $conn->query($login);
 if ($result->num_rows > 0){
     $_SESSION["login_start"] = 1;
-    header("Location: http://dbproject17.org/deliverable3/customer_home.php/");
+    header("Location: http://dbproject17.org/deliverable3/customer_home.html/");
 }
