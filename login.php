ecdbd52b27e7f5af4dd42495819bd659abf39f0e
aatalla <aatalla@andrew.cmu.edu>
2020-11-17 21:42:23 +0300

    ekvbwfgikw

--- login.php
+++ login.php
@@ -1,13 +1,22 @@
+/* aligns the buttons to the center */
+<style>?
+    .center {
+        text-align: center;
+    }
+</style>
 <?php
 
 session_start();
-$_SESSION["login_status"] = False;
+$_SESSION["login_status"] = 0;
 
 ?>
-
+<div>
 <form>
-<label> Email: </label>
-<input type="email">
-<label>Password</label>
-<input type="text>">
+    <label> Email: </label>
+    <input type="email">
+    <br>
+    <br>
+    <label>Password</label>
+    <input type="text>">
 </form>
+</div>
