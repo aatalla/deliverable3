6109641143f39e4a536f7d08bc5762f504b7f09a
OMR2K <qtr.omr@gmail.com>
2020-11-18 00:22:15 +0300

    kljiajhds

--- response.php
+++ response.php
@@ -1,3 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+
+<style>
+
+    /* footer (copyright) style */
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+
+    /* button styles */
+    .buttons {
+        color: white;
+        font-size: 24px;
+        border-radius: 8px;
+    }
+
+    /* shadow effect on button hover */
+    .buttons:hover {
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    }
+
+    /* aligns the buttons to the center */
+    .center {
+        text-align: center;
+    }
+
+</style>
+
+<head>
+<title> Worldcup 2022 - Seat Booking </title>
+</head>
+
+<body>
+
+<p style="text-align: center; font-size: 36px;"> <b>World Cup 2022 Seat Booking</b> </p>
+
+<hr>
+
+<!-- Aligning the buttons, and making them clickable -->
+<div class="center">
+    <button class="buttons"><a href="home.html">Home</a></button>
+    <br>
+    <br>
+    <br>
+</div>
+
+</body>
+
 <?php 
 
 session_start();
@@ -182,3 +236,11 @@ $msg = "Transaction ID: " . $transactionid . "\n"
 mail($email, "Transaction Information", $msg);
 */
 ?>
+
+<!-- Copyright section -->
+<footer class="footer">
+    <hr>
+    <p>Copyright Team 2X</p>
+</footer>
+
+</html>
