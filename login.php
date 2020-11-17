f9885590f85fedf795f417637b8a8c809d7954c6
aatalla <aatalla@andrew.cmu.edu>
2020-11-17 22:50:25 +0300

    glojopejgroejr

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+$_SESSION["login_status"] = 0;
+
+$servername = "dbproject5.org";
+$username = "Team2X_customer";
+$password = "Team2X_customer";
+$dbname = "Team2X_Project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$login = "SELECT UserEmail, UserPassword password FROM USER WHERE UserEmail ='" . $email . "'," . "and UserPassword='" . $password ."';";
+$result = $conn->query($login);
+if ($result->num_rows > 0){
+    $_SESSION["login_start"] = 1;
+    header("Location: http://dbproject17.org/deliverable3/customer_home.php/");
+}
