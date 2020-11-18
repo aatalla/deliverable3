c1e33ae0421adde0f9044b06e2ef14d3ab4b46a7
aatalla <aatalla@andrew.cmu.edu>
2020-11-19 01:54:31 +0300

    hjetpj

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -55,7 +55,7 @@
 <?php
 
 session_start();
-$_SESSION["login_status"] = 0;
+$_SESSION["admin_login_status"] = 0;
 
 $servername = "dbproject5.org";
 $username = "Team2X_customer";
@@ -69,15 +69,14 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$email = $_POST['admin_email'];
+$password = $_POST['admin_password'];
 
 $login = "SELECT * FROM USERS WHERE UserEmail ='" . $email . "'" . "and UserPassword='" . $password ."' and UserRole ='admin';";
 $result = $conn->query($login);
 if ($result->num_rows > 0){
-    $_SESSION["login_status"] = 1;
-    $_SESSION["email"] = $email;
-    header("location: customer_home.php");
+    $_SESSION["admin_login_status"] = 1;
+    header("location: admin_home.php");
 }
 else{
     echo "Username and Password don't match.";
