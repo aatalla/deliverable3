7ede5a1d04fb0f279c0f961949392c72a8150859
aatalla <aatalla@andrew.cmu.edu>
2020-11-19 02:19:25 +0300

    pgrewjop

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,3 +1,95 @@
+<!DOCTYPE html>
+
+<!--  
+
+    Home page - done
+    match_booking - WIP
+    seat_booking - WIP
+    ticket_info - WIP
+    confirm_order - WIP
+    credit card validation (HW1) - WIP
+    SUCCESS page - WIP
+
+    Purchases Tab
+    List all tickets bought
+
+-->
+
+<html>
+
+<style>
+
+    /* footer (copyright) style */
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+
+    /* button styles */
+    .buttons {
+        color: white;
+        font-size: 24px;
+        border-radius: 8px;
+    }
+
+    /* shadow effect on button hover */
+    .buttons:hover {
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    }
+
+    /* aligns the buttons to the center */
+    .center {
+        text-align: center;
+    }
+
+</style>
+
+<head>
+<title> Worldcup 2022 - Homepage </title>
+</head>
+
+<body>
+
+<p style="text-align: center; font-size: 36px;"> <b>World Cup 2022 Booking Website</b> </p>
+
+<button class="buttons"><a href="logout.php">Log Out</a></button>
+
+<hr>
+
+<?php 
+    session_start();
+    if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
+        header("location: admin_login_page.html");
+        return;
+    }
+
+?>
+
+<!-- Aligning the buttons, and making them clickable -->
+<div class="center">
+    <button class="buttons"><a href="customer_home.php">Home</a></button>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <button class="buttons"><a href="book.html">Match Booking</a></button>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <button class="buttons"><a href="purchases.php">Purchases</a></button>
+</div>
+
+</body>
+
+<!-- Copyright section -->
+<footer class="footer">
+    <hr>
+    <p>Copyright Team 2X</p>
+</footer>
+
+</html>
 <html>
     <a href="list_match.php">List Matches</a>
     <br/>
