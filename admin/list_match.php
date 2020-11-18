bab7ba5c4c4b50b49be8aaf9004e9ef011496a18
aatalla <aatalla@andrew.cmu.edu>
2020-11-19 00:37:48 +0300

    phjp

--- admin/list_match.php
+++ admin/list_match.php
@@ -46,7 +46,6 @@
                         echo "<td>" . $get_matches_row["StadiumName"] . "</td>";
                         
                         $get_seats_sql = "select Category1Capacity, Category2Capacity, Category3Capacity, Category4Capacity from STADIUM where StadiumName=" . "'" . $get_matches_row["StadiumName"] . "';";
-                        echo $get_seats_sql;
                         $get_capacities = $conn->query($get_seats_sql);
                         $get_cat1cap = $get_capacities->fetch_assoc()["Category1Capacity"];
                         $get_cat2cap = $get_capacities->fetch_assoc()["Category2Capacity"];
