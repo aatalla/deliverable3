1b4c136ca23ba4309f5aaa4bfbcc8681bb84db82
aatalla <aatalla@andrew.cmu.edu>
2020-11-19 22:08:31 +0300

    fhjjfyjfh

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -59,12 +59,14 @@
 
 <hr>
 
-<?php 
+<?php
+if(!isset($_SESSION)){
     session_start();
-    if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
-        header("location: admin_login_page.html");
-        return;
-    }
+}
+if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
+    header("location: login_page.html");
+    return;
+}
 
 ?>
 
--- admin/delete_match.php
+++ admin/delete_match.php
@@ -1,4 +1,11 @@
 <?php
+if(!isset($_SESSION)){
+    session_start();
+}
+if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
+    header("location: login_page.html");
+    return;
+}
     include('hw3.php');
     $servername = "dbproject17.org";
     $username = "myDBUser";
