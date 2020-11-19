f06862389049c7247378755e938f3563bf520d7e
OMR2K <qtr.omr@gmail.com>
2020-11-19 21:32:27 +0300

    alksnn

--- logout.php
+++ logout.php
@@ -2,7 +2,10 @@
 
 session_start();
 $_SESSION["login_status"] = 0;
+$_SESSION["admin_login_status"] = 0;
 $_SESSION["email"] = "";
+$_SESSION["admin_email"] = "";
+session_destroy();
 header("location: home.html");
 
 ?>
