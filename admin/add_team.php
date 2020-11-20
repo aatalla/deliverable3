76670703da409a79798b00c5b735da09149a8739
aatalla <aatalla@andrew.cmu.edu>
2020-11-20 16:17:21 +0300

    ggi

---
+++ admin/add_team.php
@@ -0,0 +1,28 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
+    header("location: login_page.html");
+    return;
+}
+include ('admin_home.php');
+$TeamName = $_POST["TeamName"];
+$servername = "dbproject5.org";
+$username = "Team2X_admin";
+$password = "Team2X_admin";
+$dbname = "Team2X_Project";
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "select StadiumName from STADIUM where StadiumName = '" . $TeamName . "';";
+$check_sql = $conn->query($sql);
+$exists_row = $check_sql->num_rows;
+if($exists_row > 0){
+    echo "A stadium with this name already exists.";
+}
+else{
+    $insert_sql = "insert into STADIUM values('" . $_POST["TeamName"] ."');";
+    $insert = $conn->query($insert_sql);
+    echo "Team added successfuly.";
+}
+
+?>
