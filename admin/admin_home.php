d1dfd2dd7d4fd0c865e6e3af3e52cfcbb1ba4f1c
aatalla <aatalla@andrew.cmu.edu>
2020-11-21 16:48:38 +0300

    gjgj

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -79,7 +79,7 @@ if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <>
   <li><a href="list_venue.php">List Venues</a></li>
   <li><a href="list_team.php">List Teams</a></li>
   <li><a href="list_ticket.php">List Tickets</a></li> 
-  <li><a href="">List Customers</a></li> <!-- to be completed -->
+  <li><a href="list_customer">List Customers</a></li> <!-- to be completed -->
   <li><a href="">List Guests</a></li> 
   <li style="float:right"><a href="../logout.php">Log Out</a></li>
 </ul>
