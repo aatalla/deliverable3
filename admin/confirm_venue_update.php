e8f6f91b4bf6cf0344b739bae7dd29bbd7d908b6
aatalla <aatalla@andrew.cmu.edu>
2020-11-21 03:39:51 +0300

    eiug

--- admin/confirm_venue_update.php
+++ admin/confirm_venue_update.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <>
     $password = "Team2X_admin";
     $dbname = "Team2X_Project";
     $conn = new mysqli($servername, $username, $password, $dbname);
-    $sql = "update Stadium set Category1Capacity =" . $_POST['Category1Capacity'] .", ". "Category2Capacity=" . $_POST['Category2Capacity'] . ",'" . "StadiumName='" . $_POST['StadiumName'] . "'," . 
+    $sql = "update Stadium set Category1Capacity =" . $_POST['Category1Capacity'] .", ". "Category2Capacity=" . $_POST['Category2Capacity'] . "," . "StadiumName='" . $_POST['StadiumName'] . "'," . 
     "StadiumAddress='" . $_POST['StadiumAddress'] . "'," . "StadiumCity='" . $_POST['StadiumCity'] . "'," .  "Category3Capacity=" . $_POST['Category3Capacity'] . "," .  "Category4Capacity=" . $_POST['Category4Capacity']. " where StadiumName='" . $_POST["StadiumName"] ."';";
     echo $sql;
     echo "Match has been successfully updated.";
