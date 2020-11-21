d211e21dd8937a4d24601658ba9a005479fbc475
aatalla <aatalla@andrew.cmu.edu>
2020-11-21 03:36:46 +0300

    bi

--- admin/confirm_venue_update.php
+++ admin/confirm_venue_update.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <>
     $dbname = "Team2X_Project";
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql = "update Stadium set Category1Capacity =" . $_POST['Category1Capacity'] .", ". "Category2Capacity=" . $_POST['Category2Capacity'] . ",'" . "StadiumName='" . $_POST['StadiumName'] . "'," . 
-    "StadiumAddress='" . $_POST['StadiumAddress'] . "', " . "StadiumCity='" . $_POST['StadiumCity'] . "," .  "Category3Capacity=" . $_POST['Category3Capacity'] . "," .  "Category4Capacity=" . $_POST['Category4Capacity']. "' where StadiumName='" . $_POST["StadiumName"] ."';";
+    "StadiumAddress='" . $_POST['StadiumAddress'] . "'," . "StadiumCity='" . $_POST['StadiumCity'] . "'," .  "Category3Capacity=" . $_POST['Category3Capacity'] . "," .  "Category4Capacity=" . $_POST['Category4Capacity']. "' where StadiumName='" . $_POST["StadiumName"] ."';";
     echo $sql;
     echo "Match has been successfully updated.";
 ?>
