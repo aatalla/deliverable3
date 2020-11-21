0b61917d83e0740ff576f9b473199654701e88bc
aatalla <aatalla@andrew.cmu.edu>
2020-11-21 08:33:35 +0300

    ;

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -25,12 +25,42 @@
         text-align: center;
     }
 
+    ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    }
+
+    li {
+    float: left;
+    }
+
+    li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    }
+
+    li a:hover {
+    background-color: #111;
+    }
+
 </style>
 <link href="https://fonts.googleapis.com/css?family=Quicksand&amp;display=swap" rel="stylesheet">
 <head>
 <title> Admin Privileges </title>
 </head>
 <body>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
 <div style="width:100%;">
     <p style="text-align: right;">
         Copyright: Team 2X
@@ -78,6 +108,8 @@ if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <>
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
     <button class="buttons"><a href="list_ticket.php">List Tickets</a></button>
+
+    
 </div>
 <br>
 <br>
