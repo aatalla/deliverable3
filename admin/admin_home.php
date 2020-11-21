6ff92781ac3e5a297f0874186557fb2565b1091e
aatalla <aatalla@andrew.cmu.edu>
2020-11-21 09:15:17 +0300

    '

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,3 +1,13 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
+    header("location: ../login_page.html");
+    return;
+
+}
+?>
 <html>
 
 <style>
@@ -19,13 +29,6 @@
     .buttons:hover {
         box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
     }
-
-    /* aligns the buttons to the center */
-    .center {
-        width: auto;
-        text-align: center;
-    }
-
     ul {
     width: 100%;
     list-style-type: none;
@@ -85,46 +88,6 @@
   <li style="float:right"><a href="../logout.php">Log Out</a></li>
 </ul>
 <hr>
-
-<?php
-if(!isset($_SESSION)){
-    session_start();
-}
-if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
-    header("location: ../login_page.html");
-    return;
-
-}
-?>
-<!-- Aligning the buttons, and making them clickable -->
-<div class="center">
-    <button class="buttons"><a href = "new_match.php">Add New Match</a></button>
-
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="new_venue.php">Add New Venue</a></button>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="new_team.php">Add New Team</a></button>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-
-    <br>
-    <br>
-
-    <button class="buttons"><a href="list_match.php">List Matches</a></button>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="list_venue.php">List Venues</a></button>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="purchases.php">List Teams</a></button>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="list_ticket.php">List Tickets</a></button>
-
-    
-</div>
 <br>
 <br>
 </body>
