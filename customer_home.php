a883cc255312ca12e44cb73fc446df2f87b86724
OMR2K <qtr.omr@gmail.com>
2020-11-21 16:33:38 +0300

    asf

--- customer_home.php
+++ customer_home.php
@@ -1,92 +1,95 @@
 <!DOCTYPE html>
 
-<!--  
-
-    Home page - done
-    match_booking - WIP
-    seat_booking - WIP
-    ticket_info - WIP
-    confirm_order - WIP
-    credit card validation (HW1) - WIP
-    SUCCESS page - WIP
-
-    Purchases Tab
-    List all tickets bought
-
--->
-
 <html>
 
-<style>
-
-    /* footer (copyright) style */
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
+    <style>
+        html{
+            color: white;
+            background-color: #8a1538;
+            font-family: 'Quicksand', sans-serif;
+        }
+        
+        ul {
         width: 100%;
-        text-align: center;
-    }
-
-    /* button styles */
-    .buttons {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #8a1538;
+        }
+    
+        li {
+        float: left;
+        }
+    
+        li a {
+        display: block;
         color: white;
-        font-size: 24px;
-        border-radius: 8px;
-    }
-
-    /* shadow effect on button hover */
-    .buttons:hover {
-        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-    }
-
-    /* aligns the buttons to the center */
-    .center {
         text-align: center;
-    }
+        padding: 14px 16px; 
+        text-decoration: none;
+        }
+    
+        li a:hover:not(.active) {
+        background-color: white;
+        border-radius: 15px;
+        color: #e2b33a;
+        }
+        
+        table,td,th{
+            text-align:center; 
+            border: 1px white solid;
+            border-radius: 15px;
+            padding: 5px;
+        }
+    
+        input{
+            border: 1px solid #e2b33a;
+            border-radius: 15px;
+        }
 
-</style>
+        select{
+            border: 1px solid #e2b33a;
+            border-radius: 15px;
+        }
 
-<head>
-<title> Worldcup 2022 - Homepage </title>
-</head>
+        .center {
+        text-align: center;
+        }
+    </style>
 
-<body>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&amp;display=swap" rel="stylesheet">
+    <head>
+    <title> Home </title>
+    </head>
 
-<p style="text-align: center; font-size: 36px;"> <b>World Cup 2022 Booking Website</b> </p>
+    <body>
 
-<button class="buttons"><a href="logout.php">Log Out</a></button>
+        <div style="width:100%;">
+            <p style="text-align: right;">
+                Copyright: Team 2X
+            </p>
+        </div>
+        <h1 style="width: 100%; text-align:center;">Register</h1>
+        <ul>
+        <li><a href="customer_home.html">Home</a></li>
+        <li><a href="book.html">Match Booking</a></li>
+        <li><a href="purchases.php">Purchases</a></li>
+        <li style="float:right"><a href="logout.php">Log Out</a></li>
+        </ul>
+        <hr>
+        <br>
+        <br>
 
-<hr>
+    <?php 
+        session_start();
+        if(!isset($_SESSION["login_status"]) || $_SESSION["login_status"] <> 1){
+            header("location: home.html");
+            return;
+        }
 
-<?php 
-    session_start();
-    if(!isset($_SESSION["login_status"]) || $_SESSION["login_status"] <> 1){
-        header("location: home.html");
-        return;
-    }
+    ?>
 
-?>
-
-<!-- Aligning the buttons, and making them clickable -->
-<div class="center">
-    <button class="buttons"><a href="customer_home.php">Home</a></button>
-
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="book.html">Match Booking</a></button>
-
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    <button class="buttons"><a href="purchases.php">Purchases</a></button>
-</div>
-
-</body>
-
-<!-- Copyright section -->
-<footer class="footer">
-    <hr>
-    <p>Copyright Team 2X</p>
-</footer>
+    </body>
 
 </html>
