250768a258f4f01752f046833600a78a14448c06
OMR2K <qtr.omr@gmail.com>
2020-11-21 21:43:12 +0300

    jbg

--- admin/add_match.php
+++ admin/add_match.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION)){
     session_start();
 }
 if(!isset($_SESSION["admin_login_status"]) || $_SESSION["admin_login_status"] <> 1){
-    header("location: ../login_page.html");
+    header("location: ../home.html");
     return;
 
 }
